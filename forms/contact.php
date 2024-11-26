6b9bf164acf34d128fdab684e3b279059c534d6a
JUAN ABELLO <128939966+juanabello219@users.noreply.github.com>
2024-11-26 03:32:01 -0500

    commit

--- forms/contact.php
+++ forms/contact.php
@@ -7,46 +7,34 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $servidor = "localhost";
-  $usuario = "root";
-  $clave = "";
-  $basededatos= "empresa";
 
-  $enlace = mysqli_connect ($servidor, $usuario, $clave, $basededatos);
-  
-  
-  
-  
-  $receiving_email_address = 'contact@example.com';
+  include("conexion.php");
+
+  if(isset($_POST['contact'])){
+
+    if(
+
+      strlen($_POST['name']) >= 1 &&
+      strlen($_POST['correo']) >= 1 &&
+      strlen($_POST['razon']) >=  1 &&
+      strlen($_POST['descripcion']) >= 1 &&
+    ){
+
+      $name = trim($_POST['name']);
+      $correo = trim($_POST['correo']);
+      $razon = trim($_POST['razon']);
+      $descripcion = trim($_POST['descripcion']);
+
+      $consulta = "INSERT INTO peticiones(nombre, correo, razon, descripcion)
+                  values ('name', 'correo', 'razon', 'descripcion', "")";
+      
+      $resultado = mysqli_query($conex)
 
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+      
+    }
+
+
+
   }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
 ?>
